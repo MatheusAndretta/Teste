79e5a7060b5cacaddd24a809e2ab8fa4d1bedbab
MatheusAndretta <matheusandretta10@gmail.com>
2023-09-26 13:32:30 -0300

    TarefaCalculo

---
+++ TarefaCalculoMedia/src/ProgramaP.java
@@ -0,0 +1,14 @@
+public class ProgramaP {
+    public static void main(String[] args) throws Exception {
+        int num1 = 8;
+        int num2 = 9;
+        int num3 = 10;
+        int num4 = 7;
+
+        int media = (num1 + num2 + num3 + num4) /4;
+        System.out.println("*****************");
+        System.out.println("A media da suas 4 notas são: " + media);
+
+
+    }
+}
