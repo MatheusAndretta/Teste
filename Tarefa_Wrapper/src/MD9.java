888bcc4f2ddcaf04112e266e19e517279abc1416
MatheusAndretta <matheusandretta10@gmail.com>
2023-09-27 12:52:11 -0300

    Tarefa

--- Tarefa_Wrapper/src/MD9.java
+++ Tarefa_Wrapper/src/MD9.java
@@ -1,5 +1,13 @@
-public class App {
+public class MD9 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        long num1 = 12312312313l;
+    
+        System.out.println(num1);
+        
+        System.out.println("*********");
+
+        int num2 = (int) num1;
+        System.out.println(num2);
     }
 }
