3d3daa29f07b66d9985822df26826b882ba8a6a4
MatheusAndretta <145785483+MatheusAndretta@users.noreply.github.com>
2023-10-23 17:10:49 -0300

    Delete Tarefa padrão de projeto 1/FactoryB directory

--- Tarefa padrão de projeto 1/FactoryB/src/Factory/CorolaCar.java
+++
@@ -1,9 +0,0 @@
-package Factory;
-
-public class CorolaCar extends Car {
-
-    public CorolaCar(int horsePower, String fuelSource, String color) {
-        super(horsePower, fuelSource, color);
-    }
-    
-}
--- Tarefa padrão de projeto 1/FactoryB/src/Factory/Fusca.java
+++
@@ -1,8 +0,0 @@
-package Factory;
-
-public class Fusca extends Car{
-
-    public Fusca(int horsePower, String fuelSource, String color) {
-        super(horsePower, fuelSource, color);
-    }
-}
